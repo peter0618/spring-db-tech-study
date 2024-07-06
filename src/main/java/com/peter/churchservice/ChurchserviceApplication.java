f38b9226ae3fc03025cc80da2193b7f325c39752
peter0618 <peterchoi0618@gmail.com>
2024-07-07 02:15:56 +0900

    first commit

---
+++ src/main/java/com/peter/churchservice/ChurchserviceApplication.java
@@ -0,0 +1,13 @@
+package com.peter.churchservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChurchserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChurchserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/peter/churchservice/ChurchserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peter.churchservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChurchserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
